ee6a97a5a1ff80b83d156d52b2f16f5b8b80aabf
Beelmann, Marko <marko.beelmann@philips.com>
2023-04-24 09:00:35 +0200

    Run code when PR is closed

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { GetPullRequestData } from './GitHubCliHelper'
 
 type Inputs = {
   name: string
@@ -6,6 +8,23 @@ type Inputs = {
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<number> => {
-  core.info(`my name is ${inputs.name}`)
+  
+  // take care that action is running only in PR context
+  if (process.env.GITHUB_EVENT_NAME !== 'pull_request') {
+    core.setFailed('Action is running outside of PR context')
+    return 0
+  }
+  // ensure that the action only runs if PR is closed
+  if (process.env.GITHUB_EVENT_ACTION !== 'closed') {
+    core.setFailed('Action is running outside of PR closed context')
+    return 0
+  }
+
+  // get PR number
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+  const cliPullRequestData = await GetPullRequestData(github.context.issue.number)
+  core.info(JSON.stringify(cliPullRequestData))
+
+//  core.info(`my name is ${inputs.name}`)
   return 12
 }
